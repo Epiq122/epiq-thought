1dce2406e67ccf900b013859776c5a3c840f716d
Robert Gleason <l2ob122@gmail.com>
2024-02-04 20:59:56 -0800

    Add URLs and views for Thoughtify app

--- epiqthought/thoughtify/views.py
+++ epiqthought/thoughtify/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def home(request):
+    return render(request, "thoughtify/home.html")
+
+
+def register(request):
+    return render(request, "thoughtify/register.html")
+
+
+def login(request):
+    return render(request, "thoughtify/login.html")
+
+
+def dashboard(request):
+    return render(request, "thoughtify/dashboard.html")
