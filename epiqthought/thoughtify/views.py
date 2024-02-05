7f7e225fa30544883c3abc4529bcc83da2ae96bd
Robert Gleason <l2ob122@gmail.com>
2024-02-04 21:04:26 -0800

    Add dashboard, index, login, and register HTML templates

--- epiqthought/thoughtify/views.py
+++ epiqthought/thoughtify/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request, "thoughtify/home.html")
+    return render(request, "thoughtify/index.html")
 
 
 def register(request):
